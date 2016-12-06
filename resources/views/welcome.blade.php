ab735fab02ac886c4b11bebf037be089a4a8e0de
smallnews <smallnews@sn.com>
2016-12-06 17:09:21 +0800

    path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
         </div>
 
         <div class="tm-home-img-container">
-            <img src="{{ asset('/testblog/img/tm-home-img.jpg') }}" alt="Image" class="hidden-lg-up img-fluid">
+            <img src="/testblog/img/tm-home-img.jpg" alt="Image" class="hidden-lg-up img-fluid">
         </div>
 
         <section class="tm-section">
