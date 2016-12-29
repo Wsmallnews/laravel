78dfd2bb2100bd385b9080879acdeff4372d89f8
smallnews <smallnews@sn.com>
2016-12-29 17:49:57 +0800

    github login

--- app/Http/Controllers/Desktop/Auth/LoginController.php
+++ app/Http/Controllers/Desktop/Auth/LoginController.php
@@ -34,7 +34,7 @@ class LoginController extends Controller
      * @return void
      */
     public function __construct()
-    {
+    {print_r('123123123');exit;
         $this->middleware('guest', ['except' => 'logout']);
     }
     
