c02ec86bbeaa9ac2d372bf2da72b7515d1723b25
smallnews <smallnews@sn.com>
2017-01-04 15:56:18 +0800

    write topic

--- database/seeds/TopicClassifyTableSeeder.php
+++ database/seeds/TopicClassifyTableSeeder.php
@@ -11,16 +11,16 @@ class TopicClassifyTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('topic_classifys')->insert([
+        DB::table('topic_classify')->insert([
             'name' => '教程',
         ]);
-        DB::table('topic_classifys')->insert([
+        DB::table('topic_classify')->insert([
             'name' => '分享',
         ]);
-        DB::table('topic_classifys')->insert([
+        DB::table('topic_classify')->insert([
             'name' => '问答',
         ]);
-        DB::table('topic_classifys')->insert([
+        DB::table('topic_classify')->insert([
             'name' => '招聘',
         ]);
     }
