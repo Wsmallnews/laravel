0d60afe50cff464fe5fddfdf26a227409c4bfb25
smallnews <smallnews@sn.com>
2017-05-17 15:10:22 +0800

    调试 上传图片

--- app/Http/Controllers/Desktop/IndexController.php
+++ app/Http/Controllers/Desktop/IndexController.php
@@ -30,8 +30,14 @@ class IndexController extends CommonController
      */
     public function index()
     {
-        $result = MyUpload::uploadCopy("http://tvax2.sinaimg.cn/crop.0.0.120.120.180/e544b016ly8fdteyqp8xtj203c03cq51.jpg", 'avatars');
-        print_r($result);exit;
+        if ($data = @file_get_contents("http://tvax2.sinaimg.cn/crop.0.0.120.120.180/e544b016ly8fdteyqp8xtj203c03cq51.jpg", false, $context)) {
+            var_dump($data);
+            echo "---------------------------";
+            var_dump(imagecreatefromstring($data));
+        }
+        exit;
+        // $result = MyUpload::uploadCopy("", 'avatars');
+        // print_r($result);exit;
         
         return view('desktop.index');
     }
