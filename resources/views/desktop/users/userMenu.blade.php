26b12a8d9ebf71383715566f0d95716707334e98
smallnews <smallnews@sn.com>
2017-05-23 17:21:21 +0800

    修改第三方登录

--- resources/views/desktop/users/userMenu.blade.php
+++ resources/views/desktop/users/userMenu.blade.php
@@ -1,5 +1,5 @@
 <div class="list-group shadow">
     <a href="{{ route('user.edit', ['id' => $user->id]) }}" class="list-group-item {{ Route::currentRouteName() == 'user.edit' ? 'active' : ''}} ">资料修改</a>
-    <a href="{{ route('user.bind') }}" class="list-group-item pjax-element {{ Route::currentRouteName() == 'user.bind' ? 'active' : ''}}">账号绑定</a>
+    <a href="{{ route('user.bind') }}" class="list-group-item {{ Route::currentRouteName() == 'user.bind' ? 'active' : ''}}">账号绑定</a>
     {{-- <a href="#" class="list-group-item">Morbi leo risus</a> --}}
 </div>
