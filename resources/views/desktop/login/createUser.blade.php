f177ceb7f0209921344c77e361b77eb34a834847
smallnews <smallnews@sn.com>
2017-06-02 14:23:33 +0800

    login

--- resources/views/desktop/login/createUser.blade.php
+++ resources/views/desktop/login/createUser.blade.php
@@ -1,11 +1,6 @@
 @extends('desktop.layouts.app')
 
 @section('content')
-    
-    {{ print_r(session('socialiteUser')) }}
-    
-    {{ session('socialiteUser.email') }}
-    {{ session('socialiteUser.token') }}
 <div class="rows">
     <div class="col-lg-3">
         <div class="shadow mr-bo-20">asdfasdfasdf</div>
@@ -17,7 +12,7 @@
                 <form class="form-horizontal" role="form" method="POST" action="{{ url('/createUser') }}">
                     {{ csrf_field() }}
                     <input type="hidden" name="driver" @if (old('driver')) value="{{old('driver')}}" @else value="{{ session('driver') }}" @endif>
-                    <input type="hidden" name="token" @if (old('token')) value="{{old('token')}}" @else value="{{ session('socialiteUser')->token }}" @endif>
+                    <input type="hidden" name="token" @if (old('token')) value="{{old('token')}}" @else value="{{ session('socialiteUser.token') }}" @endif>
                     <div class="form-group">
                         <label for="avatar" class="col-md-4 control-label">头像</label>
                         <div class="col-md-6">
@@ -28,7 +23,7 @@
                     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                         <label for="name" class="col-md-4 control-label">用户名</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="" @if (old('name')) value="{{old('name')}}" @else value="{{ session('socialiteUser')['name'] }}" @endif>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="" @if (old('name')) value="{{old('name')}}" @else value="{{ session('socialiteUser.nickname') }}" @endif>
                             
                             @if ($errors->has('name'))
                                 <span class="help-block">
